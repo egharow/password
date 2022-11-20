5bc0b975b06547348a901db89f3b73f719fc12d2
Ely Harow <elyharow@gmail.com>
2022-11-20 23:55:18 +0200

    added usestate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       another line
-      added another line
+      <p>added another line</p>
+      <Counter/>
     </div>
   );
 }
