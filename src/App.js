a57d2628f6970a0039fd87647e0adf0066cc1639
Ely Harow <elyharow@gmail.com>
2022-12-13 13:25:51 +0200

    2nd pass

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import './App.css';
-import Counter from './Counter';
-import { useState} from 'react';
+import { useState, useEffect } from 'react';
+import password from './password';
 
+ function App() {
+  const [pass , setPass] = useState("")
 
-function App() {
-  const [delta, setDelta] = useState(1)
-  const [max, setMax] = useState(10)
-  const [restart, setReset] = useState(false)
-  
-  function handleDelta (event){
-  console.log(event);
-  setDelta(Number(event.target.value))
-  } 
-  
-  function handleMax (m){
-    console.log(m);
-    setMax((m.target.value))}
- 
-    function getReset(data){
-    console.log(data)
-    setReset(data)
+  function checkPass(event){
+    console.log(event)
+    /*setStrength(event.target.value)*/
   }
-  return (
+  return(
     <div className="App">
-      Delta
-      <input type= "number" value= {delta} onChange = {handleDelta}/>
-      <p/>Maximum Number
-      <input type= "number" value= {max} onChange={handleMax} />
-      <Counter delta = {delta} max ={max} getReset={getReset} needToReset={restart}/>
-      <Counter delta = {delta} max ={max} getReset={getReset} needToReset={restart}/>
-    </div>
+      Enter Password:
+    <input type="password" /*value={pass}*/ onChange= {checkPass} /> 
     
-  );
+    <button>Submit</button>
+    </div>
+    );
 }
 
 export default App;
